2152ce0928d5ca8ce8beb3b04e2a240809aeace1
WoodwindCDT <woodwind.turbeville@gmail.com>
2021-08-10 18:49:28 -0500

    Added new filtering functions in Alg.js, but further testing needed. Also, added new data to JSON to allow testing of java vs javaScript

--- src/components/Algorithms/index.js
+++ src/components/Algorithms/index.js
@@ -1,8 +1,65 @@
-import React from "react";
-import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
+import ReactDOM from "react-dom";
+import React, { Component } from "react";
 import { algorithms } from "../JSON/alg";
 
-const C = (h, i) => {
+// class App extends React.Component {
+//     constructor(props) {
+//       super(props);
+  
+//       this.state = {
+//         filterCode: [],
+//         probs: props.probs
+//     };
+// }
+
+// onResetArray = () => {
+//     this.setState({ filterCode: [] });
+// };
+
+// filterJava = () => {
+//     const { probs } = this.state;
+//     const java = probs.filter(code => code.T.includes("java"));
+//     this.setState({ filterCode: java });
+// };
+
+// filterJavaScript = () => {
+//     const { probs } = this.state;
+//     const javascript = probs.filter(code => code.T.includes("javascript"));
+//     this.setState({ filterCode: javascript });
+// };
+
+// render() {
+//     const {probs, filterCode} = this.state;
+//         return (
+//         <div className="container">
+//             <button event-key="reset-probs" onClick={this.onResetArray}>
+//             All Code
+//             </button>
+
+//             <button event-key="javascript-probs" onClick={this.filterJavaScript}>
+//             JavaScript
+//             </button>
+
+//             <button event-key="java-probs" onClick={this.filterJava}>
+//             Java
+//             </button>
+
+//             <Algorithms probs={filterCode.length > 0 ? filterCode : probs} />
+//         </div>
+//         );
+//     }
+// }
+
+// const Algorithms = ({ probs }) => {
+//     return (
+//         <div id="algo" className="t-container flex-row">
+//         <h1 className="t-head">Practice Algorithm Questions w/ Answers</h1>
+//             {probs.map(algorithms => <div>{algorithms.T}</div>)}
+//         </div>
+//     );
+// }
+
+const A = (h, i, x) => {
     return(
         <div key={(i)}>
             <div className="t-container1">
@@ -15,16 +72,17 @@ const C = (h, i) => {
                         A: {h.A}
                     </p>
                 </div>
+                {h.T}
             </div>
         </div>
     );
 };
 
-function Algorithms() {
+const Algorithms = ({ probs }) => {
     return (
         <div id="algo" className="t-container flex-row">
         <h1 className="t-head">Practice Algorithm Questions w/ Answers</h1>
-          {algorithms.map(C)}
+        {algorithms.map(A)}
         </div>
     );
 }
--- src/components/JSON/alg.js
+++ src/components/JSON/alg.js
@@ -69,23 +69,23 @@ let newStr = '';
 console.log('Largest int = ' + Math.max.apply(Math, arr), arr) }
     scan();` 
     },
-    // {   
-    //     "T": "java",
-    //     "Q": "Write a function that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.",
-    //     "A": `class Main {
-    //         public static void main(String[] args) {
-    //            for (int i = 1; i < 101; i++) {
-    //                 if ((i % 15) == 0 )
-    //                 System.out.println("FizzBuzz");
-    //                 if((i % 3) == 0)
-    //                 System.out.println("Fizz");
-    //                 if((i % 5) == 0)
-    //                 System.out.println("Buzz");
-    //                 else System.out.println(i);
-    //            }
-    //         }
-    //     }`
-    // },
+    {
+         "T": "java",
+         "Q": "Write a function that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.",
+         "A": `class Main {
+             public static void main(String[] args) {
+                for (int i = 1; i < 101; i++) {
+                     if ((i % 15) == 0 )
+                     System.out.println("FizzBuzz");
+                     if((i % 3) == 0)
+                     System.out.println("Fizz");
+                     if((i % 5) == 0)
+                     System.out.println("Buzz");
+                     else System.out.println(i);
+                }
+             }
+         }`
+    },
     // {   
     //     "T": "java",
     //     "Q": "Create a chessboard with \" \" (a space) for even and \"$\" for odd integers",
